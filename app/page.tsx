a4a6bb7505a34b508f9fd78e59f2dfb95b11ebe3
Scott Motte <mot@mot.la>
2025-02-24 10:47:25 -0800

    decryption-at-access

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export const dynamic = 'force-dynamic';
 
+import * as dotenvx from "@dotenvx/dotenvx";
+
 export default function Page() {
-  return <h1>Hello, {process.env.HELLO}</h1>;
+  return <h1>Hello, {dotenvx.get('HELLO')}</h1>;
 }
