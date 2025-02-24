c1cec4f44ad66db61e0c875e72c9d29066a522e9
Scott Motte <mot@mot.la>
2025-02-24 11:22:24 -0800

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,5 @@ export const dynamic = 'force-dynamic';
 import * as dotenvx from "@dotenvx/dotenvx";
 
 export default function Page() {
-  // @ts-ignore
   return <h1>Hello, {dotenvx.get('HELLO')}</h1>;
 }
